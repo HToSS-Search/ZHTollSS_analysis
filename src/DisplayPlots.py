8a44f0cc211aaf3beb5b410fdacdc3617dba48d1
YolanVekemans <yvk9940@gmail.com>
2024-11-20 17:38:41 +0100

    Improved seperation of analysis and plotting scripts. Redefined selection cuts. Made Z mass window symmetric.

--- src/DisplayPlots.py
+++ src/DisplayPlots.py
@@ -2,17 +2,114 @@ import ROOT
 import numpy as np
 
 
-outFile = ROOT.TFile("plots/UL2017/MC.root", "READ")
+#Custom object which contains name of object and object to be displayed
+class displayObj:
+    def __init__(self, objStr, obj):
+        self.name = objStr
+        self.obj = obj
 
-i = 0
-canvasNameList = ["canvas_turnon_HLT_IsoMu27", "canvas_turnon_HLT_IsoMu24", "canvas_turnon_HLT_IsoTkMu24", "canvas_pt", "canvas_mu_eta", "canvas_mu_phi", "canvas_deltaR", "canvas_invmass"]
-print("Following canvasses will be displayed")
-print(canvasNameList)
-canvasList = []
-for canvasName in canvasNameList:
-	c = outFile.Get(canvasName)
-	c.Draw()
-	c.Update()
+#Helper functions for canvas generation
+def displayTurnOn(turnon, triggerName):
+    canvas = ROOT.TCanvas('canvas_turnon_' + triggerName, "Turn On Curve " + triggerName, 800, 600)
+    canvas.SetGrid()
+    turnon.Draw("AP")
+    xpos = 0
+    if triggerName[-2:].isdigit():
+        xpos = int(triggerName[-2:])
+        vLine = ROOT.TLine(xpos, canvas.GetUymin(), xpos, canvas.GetUymax())
+        vLine.SetLineStyle(2)
+        vLine.SetLineWidth(2)
+        vLine.SetLineColor(ROOT.kBlue)
+        vLine.Draw("same")
+    else:
+        None
+
+    return canvas
+    
+def displayHist(hist, varName):
+    canvas = ROOT.TCanvas('canvas_hist_' + varName, 'Muon ' + varName + ' Distribution', 800, 600)
+    hist.Draw()
+
+    return canvas
+
+def displayPTHist(listOfPt):
+    canvas = ROOT.TCanvas('canvas_hist_leadingsubleadingpt', 'Muon pT Distribution', 800, 600)
+    i = 0
+    for hist in listOfPt:
+        if (i==1):
+            hist.Draw("same")
+        else:
+            hist.Draw()
+        i+=1
+    return canvas
+
+def displayObjects(listToDisplay):
+    canvasList = []
+    leadingSubleading = []
+    #Only display the selected objects by the user
+    for displayObj in listToDisplay:
+    #Different implementations needed for different types of plots
+        if 'turnon' in displayObj.name:
+            idx_ = displayObj.name.find('_')
+            triggerName = displayObj.name[idx_:]
+            canvasList.append(displayTurnOn(displayObj.obj, triggerName))
+        elif ('hist' in displayObj.name):
+            idx_ = displayObj.name.find('_')
+            idx2_ = displayObj.name.find("_", idx_ + 1)
+            idx3_= displayObj.name.find('_', idx2_ + 1)
+            particleName = displayObj.name[idx_ + 1:idx2_]
+            varName = displayObj.name[idx2_+1:idx3_]
+            if (varName == 'pT') and ('leading' in displayObj.name):
+                leadingSubleading.append(displayObj.obj)
+                #Only if there are two histograms (leading and subleading pTs) draw them on canvas
+                if (len(leadingSubleading) == 2):
+                    canvasList.append(displayPTHist(leadingSubleading))
+                    #Remove the already drawn pair of leading and subleading histograms from list ST other leading/subleading can be drawn together in the correct pairs
+                    leadingSubleading = []
+                else: None
+            else: 
+                canvasList.append(displayHist(displayObj.obj, varName))
+        #ADD OTHER ELIFS LATER FOR OTHER PLOT IMPLEMENTATION
+        else: None
+    return canvasList       
+
+#Open the ROOT File and print out content for user
+outFile = ROOT.TFile("plots/UL2017/MC.root", "READ")
+keys = outFile.GetListOfKeys()
+keynames = [key.GetName() for key in keys]
+print("\nThe objects inside the .root are the following:\n" + str(keynames) + "\n")
+
+#Ask for user input which plots want to be seen
+user_input = input(
+    f"Please specify all objects to be displayed (comma-separated). Type 'all' to display all loaded objects\n"
+    )
+
+#Generate and add to canvasList accordingly
+inputDisplayList = []
+listToDisplay = []
+if (user_input.lower() == 'all'):
+    #if all selected use the complete list
+    #A list of custom display objects is generated which takes the obj name and obj itself from the .root file
+    i = 0
+    for key in keys:
+        objName = key.GetName()
+        listToDisplay.append(displayObj(objName, outFile.Get(objName)))    
+        i+=1
+else:
+    inputDisplayList = [obj.strip() for obj in user_input.split(",")]
+    #Based on the list of selected names of objects by user take objects from .root accordingly
+    i = 0
+    for inputObjName in inputDisplayList:
+        listToDisplay.append(displayObj(inputObjName, outFile.Get(inputObjName)))
+        i += 1
+
+canvasList = displayObjects(listToDisplay)
+
+#Draw and update the canvasses
+for canvas in canvasList:
+    canvas.Update()
+    canvas.Draw()
+
 
 outFile.Close()
 print("\n Program running... Press Enter to stop.")
--- src/RDataFrames_Zpeak_Yolan.py
+++ src/RDataFrames_Zpeak_Yolan.py
@@ -20,23 +20,23 @@ using FourVector = ROOT::Math::PxPyPzMVector;
 using RVecFloat = ROOT::RVec<Float_t>;
 
 float getLeading(RVecFloat vec){
-	auto idxmax = ROOT::VecOps::ArgMax(vec);
-	return vec[idxmax];
+    auto idxmax = ROOT::VecOps::ArgMax(vec);
+    return vec[idxmax];
 
 }
 
 float getTrailing(RVecFloat vec){
-	auto idxmin = ROOT::VecOps::ArgMin(vec);
-	return vec[idxmin];
+    auto idxmin = ROOT::VecOps::ArgMin(vec);
+    return vec[idxmin];
 }
 auto myInvariantMass(RVecFloat& pt, RVecFloat& eta, RVecFloat& phi, float mass){
         Float_t px1 = pt[0]*cos(phi[0]);Float_t py1 = pt[0]*sin(phi[0]);Float_t pz1 = pt[0]*cos(eta[0]);
-	Float_t px2 = pt[1]*cos(phi[1]);Float_t py2 = pt[1]*sin(phi[1]);Float_t pz2 = pt[1]*cos(eta[1]);
-	FourVector P1 {px1, py1, pz1, mass};
-	FourVector P2 {px2, py2, pz2, mass};
-	FourVector dilep = P1 + P2;
-	auto mass_ = dilep.M();
-	return mass_;
+    Float_t px2 = pt[1]*cos(phi[1]);Float_t py2 = pt[1]*sin(phi[1]);Float_t pz2 = pt[1]*cos(eta[1]);
+    FourVector P1 {px1, py1, pz1, mass};
+    FourVector P2 {px2, py2, pz2, mass};
+    FourVector dilep = P1 + P2;
+    auto mass_ = dilep.M();
+    return mass_;
 }
 
 RVecFloat getDeltaR(RVecFloat& eta, RVecFloat& phi){
@@ -44,19 +44,19 @@ RVecFloat getDeltaR(RVecFloat& eta, RVecFloat& phi){
  * Input should be the relevant eta and phi columns respectively
  * Returns Vec with length equal to amount of combinations where each entry is the DeltaR for a pair
  */
-	if (eta.size() != phi.size()) {
-		std::cout << "Eta and phi are not the same size!" << std::endl;
-		return RVecFloat();
-	} else {
-		auto idx = ROOT::VecOps::Combinations(eta, 2);
-		RVecFloat deltaR(idx[0].size()); // Initialize deltaR with the correct size
-					           
-		auto phi1 = Take(phi, idx[0]); auto phi2 = Take(phi, idx[1]);
-		auto eta1 = Take(eta, idx[0]); auto eta2 = Take(eta, idx[1]);
+    if (eta.size() != phi.size()) {
+        std::cout << "Eta and phi are not the same size!" << std::endl;
+        return RVecFloat();
+    } else {
+        auto idx = ROOT::VecOps::Combinations(eta, 2);
+        RVecFloat deltaR(idx[0].size()); // Initialize deltaR with the correct size
+                               
+        auto phi1 = Take(phi, idx[0]); auto phi2 = Take(phi, idx[1]);
+        auto eta1 = Take(eta, idx[0]); auto eta2 = Take(eta, idx[1]);
 
-		deltaR = ROOT::VecOps::DeltaR(eta1, eta2, phi1, phi2);
-		return deltaR;
-	}
+        deltaR = ROOT::VecOps::DeltaR(eta1, eta2, phi1, phi2);
+        return deltaR;
+    }
 
 }
 """
@@ -98,13 +98,13 @@ luminosity = 1 if 'Run' in args.config else givenLuminosity
 #Lets perform a check
 weightPlot = dataFile.Get("jmeanalyzer/h_Counter").Clone()
 if sumWeights != weightPlot.GetBinContent(1):
-	sumWeights = weightPlot.GetBinContent(1)
-	print("\nSum of weights in config file did not match actual sum of weights, change the value in the config file accordingly")
-	print("\ncorrect sum of weights is: {} ".format(sumWeights))
+    sumWeights = weightPlot.GetBinContent(1)
+    print("\nSum of weights in config file did not match actual sum of weights, change the value in the config file accordingly")
+    print("\ncorrect sum of weights is: {} ".format(sumWeights))
 else:
-	None
-	print("\nSum of weights in config matches sum of weights calculated from simulation")
-	print("\ncorrect sum of weights is: {} ".format(sumWeights))
+    None
+    print("\nSum of weights in config matches sum of weights calculated from simulation")
+    print("\ncorrect sum of weights is: {} ".format(sumWeights))
 
 
 #########################################################################################
@@ -113,87 +113,69 @@ else:
 
 
 def genTurnOn(df, triggerName, mmin, mmax, steps):
-	#Filter out the muons from the dataset before applying trigger to get a better gauge on trigger efficiency
-	#If this is not done, max efficiency plateau at 50% due to presence of electrons
-	df_muons = df.Filter('_nEles == 0')
-	df_muontrigger = df_muons.Filter(triggerName)
-	
-	pTBins = np.arange(mmin,mmax,steps)
+    #Filter out the muons from the dataset before applying trigger to get a better gauge on trigger efficiency
+    #If this is not done, max efficiency plateau at 50% due to presence of electrons
+    df_muons = df.Filter('_nEles == 0')
+    df_muontrigger = df_muons.Filter(triggerName)
+    
+    #Get the leading muon pT since this is most likely the muon the event trigger triggered on
+    df_muons = df_muons.Define('leadingPt', 'getLeading(_lPt)')\
+            .Define('subleadingPt', 'getTrailing(_lPt)')
+    df_muontrigger = df_muontrigger.Define('leadingPt', 'getLeading(_lPt)')\
+            .Define('subleadingPt', 'getTrailing(_lPt)')
+    
+    pTBins = np.arange(mmin,mmax,steps)
 
-	eff_hist = df_muontrigger.Histo1D(('triggered_muons', 'Triggered muons', len(pTBins)-1, pTBins), '_lPt')
-	eff_hist_total = df_muons.Histo1D(('total_muons', 'Total  muons', len(pTBins)-1, pTBins), '_lPt')
+    eff_hist = df_muontrigger.Histo1D(('hist_triggerleading', 'Triggered muons', len(pTBins)-1, pTBins), 'leadingPt')
+    eff_hist_total = df_muons.Histo1D(('hist_totalleading', 'Total  muons', len(pTBins)-1, pTBins), 'leadingPt')
 
-	canvas = ROOT.TCanvas('canvas_turnon_' + triggerName, "Turn On Curve " + triggerName, 800, 600)
-	canvas.SetGrid()
-
-	turnon = ROOT.TEfficiency(eff_hist.GetValue(), eff_hist_total.GetValue())
-	turnon.SetTitle("Turn on curve " + triggerName + ";Muon pT (GeV);Efficiency")
-	turnon.Draw("AP")
-	
-	#Print the maximum reached efficiency
-	max_efficiency = 0
-	for i in range(1, turnon.GetTotalHistogram().GetNbinsX() + 1):
-	    if turnon.GetEfficiency(i) > max_efficiency:
-	            max_efficiency = turnon.GetEfficiency(i)
-	print("HERE")
-	print(triggerName + " Maximum Efficiency:" + str(max_efficiency))
-	
-	#Generate a vertical line at the trigger threshold
-	xpos = 0
-	if triggerName[-2:].isdigit():
-		xpos = int(triggerName[-2:])
-	vLine = ROOT.TLine(xpos, 0, xpos, canvas.GetUymax())
-	vLine.SetLineStyle(2)
-	vLine.SetLineWidth(2)
-	vLine.SetLineColor(ROOT.kBlue)
-	vLine.Draw()
-
-	canvas.Write('canvas_turnon_' + triggerName)
+    turnon = ROOT.TEfficiency(eff_hist.GetValue(), eff_hist_total.GetValue())
+    turnon.SetName("turnon_" + triggerName)
+    turnon.SetTitle("Turn on curve " + triggerName + ";Muon pT (GeV);Efficiency")
+    
+    #Print the maximum reached efficiency
+    max_efficiency = 0
+    for i in range(1, turnon.GetTotalHistogram().GetNbinsX() + 1):
+        if turnon.GetEfficiency(i) > max_efficiency:
+                max_efficiency = turnon.GetEfficiency(i)
+    print(triggerName + " Maximum Efficiency:" + str(max_efficiency))
+    
+    turnon.Write()
 
 def genPtPlot(df, mmin, mmax, bins):
+    df_leading_subleading = df.Define('leadingPt', 'getLeading(mu_pt)')\
+        .Define('subleadingPt', 'getTrailing(mu_pt)')
+    # Histograms
+    mu_leadingPt_hist = df_leading_subleading.Histo1D(('hist_mu_pT_leading', 'Leading Muon PTs', bins, mmin, mmax), 'leadingPt')
+    mu_subleadingPt_hist = df_leading_subleading.Histo1D(('hist_mu_pT_subleading', 'Subleading Muon PT', bins, mmin, mmax), 'subleadingPt')
 
-	df_leading_subleading = df.Define('leadingPt', 'getLeading(mu_pt)')\
-			.Define('subleadingPt', 'getTrailing(mu_pt)')
-	
-	# Create a canvas to plot the histograms
-	canvas = ROOT.TCanvas("canvas_pt", "Muon PT Distributions", 800, 600)
+    mu_leadingPt_hist.GetXaxis().SetTitle("Muon pT (GeV)")  # X-axis label
+    mu_leadingPt_hist.GetYaxis().SetTitle("Events")         # Y-axis label
 
-	# Histograms
-	mu_leadingPt_hist = df_leading_subleading.Histo1D(('mu_leadingpt_hist', 'Leading Muon PTs', bins, mmin, mmax), 'leadingPt')
-	mu_subleadingPt_hist = df_leading_subleading.Histo1D(('mu_subleadingpt_hist', 'Subleading Muon PT', bins, mmin, mmax), 'subleadingPt')
+    mu_leadingPt_hist.SetLineColor(ROOT.kBlue)  # Set color for the leading pt histogram
+    mu_subleadingPt_hist.SetLineColor(ROOT.kRed)  # Set color for the subleading pt histogram
 
-	mu_leadingPt_hist.GetXaxis().SetTitle("Muon pT (GeV)")  # X-axis label
-	mu_leadingPt_hist.GetYaxis().SetTitle("Events")         # Y-axis label
+    mu_leadingPt_hist.Write()
+    mu_subleadingPt_hist.Write()
 
-	mu_leadingPt_hist.SetLineColor(ROOT.kBlue)  # Set color for the leading pt histogram
-	mu_subleadingPt_hist.SetLineColor(ROOT.kRed)  # Set color for the subleading pt histogram
-
-	mu_leadingPt_hist.Draw()
-	mu_subleadingPt_hist.Draw("same")  # "same" will overlay it on the existing plot
-
-	canvas.Write('canvas_pt')
 
 def genXPlot(df, varName, mmin, mmax, bins):
-	canvas = ROOT.TCanvas('canvas_' + varName, varName + " Distribution", 800, 600)
 
-	hist = df.Histo1D(('hist_' + varName, varName, bins, mmin, mmax), varName)
-	hist.GetXaxis().SetTitle(varName)
-	hist.GetYaxis().SetTitle("Events")
+    hist = df.Histo1D(('hist_' + varName + '_', varName, bins, mmin, mmax), varName)
+    hist.GetXaxis().SetTitle(varName)
+    hist.GetYaxis().SetTitle("Events")
 
-	hist.Draw()
-	
-	canvas.Write('canvas_' + varName)
+    hist.Write()
+    
 
 def genInvMassPlot(df, mmin, mmax, bins):
-	canvas = ROOT.TCanvas('canvas_invmass', 'Invariant Z Mass', 800, 600)
 
-	hist = df.Histo1D(('hist_invmass', 'Invariant mass distribution of Z', bins, mmin, mmax), 'dimuon_mass', 'evt_weight')
-	hist.GetXaxis().SetTitle('m#_{\mu\mu} (GeV)')
-	hist.GetYaxis().SetTitle('Events')
+    hist = df.Histo1D(('hist_mu_invmass_', 'Invariant mass distribution of Z', bins, mmin, mmax), 'dimuon_mass', 'evt_weight')
+    hist.GetXaxis().SetTitle('m#_{\mu\mu} (GeV)')
+    hist.GetYaxis().SetTitle('Events')
 
-	hist.Draw()
+    hist.Write()
 
-	canvas.Write('canvas_invmass')
 
 #########################################################################################
 # Loading in the Tree and performing selection on Dataframe
@@ -215,14 +197,20 @@ print(totalEntries)
 df_muons = df.Filter('_nEles == 0', 'Filter out electrons')
 df_muontrigger = df_muons.Filter('HLT_IsoMu27', 'MuonTriggerCut')
 
-#perform a cut
-muon_cuts = 'abs(_lEta) < 2.4 && _lPt > 10 && _lPassTightID'
+#SKIM INCLUDES:
+#pT > 20, _lPassTightId, events only 2 leptons, m_ll < 110 GeV
+
+#Perform a cut
+#eta < 2.4
+#_lPassTightID
+muon_cuts = 'abs(_lEta) < 2.4 && _lPassTightID'
 df_muons_aftercut = df_muontrigger.Define('mu_pt', f'_lPt[{muon_cuts}]')\
-			.Define('mu_eta', f'_lEta[{muon_cuts}]')\
-			.Define('mu_phi', f'_lPhi[{muon_cuts}]')\
-			.Define('mu_ch',f'_lpdgId[{muon_cuts}]/13')
+            .Define('mu_eta', f'_lEta[{muon_cuts}]')\
+            .Define('mu_phi', f'_lPhi[{muon_cuts}]')\
+            .Define('mu_ch',f'_lpdgId[{muon_cuts}]/13')
 
 #perform a selection
+#Leading pT > 30 GeV (turnon at 27), oppositely charged, 2 muons
 muon_sel = "mu_pt.size() == 2 && mu_ch[0]*mu_ch[1] < 0 && Max(mu_pt) > 30"
 df_muons_aftercutselection = df_muons_aftercut.Filter(muon_sel)
 
@@ -231,77 +219,96 @@ displayList2 = ['mu_eta', 'mu_phi', 'mu_ch', 'mu_pt', '_lPt', 'HLT_IsoMu27']
 df_muons_aftercutselection.Describe().Print()
 df_muons_aftercutselection.Display(displayList2).Print()
 
-#get the dimuon mass and put into a df
+#get the dimuon mass and put into a df, mass window need be symmetric around ~90GeV
 llim_zmass = 70
-ulim_zmass = 130
+ulim_zmass = 110
 muon_massVal = 0.105 #GeV
 
 dimuon_masscut = 'dimuon_mass > ' + str(llim_zmass) + ' &&  dimuon_mass < '+ str(ulim_zmass)
 
 df_dimuon = df_muons_aftercutselection.Define('mu_mass', str(muon_massVal))\
-		.Define('dimuon_mass', 'myInvariantMass(mu_pt, mu_eta, mu_phi, mu_mass)')\
-		.Filter(dimuon_masscut)
+        .Define('dimuon_mass', 'myInvariantMass(mu_pt, mu_eta, mu_phi, mu_mass)')\
+        .Filter(dimuon_masscut)
 df_dimuon = df_dimuon.Define('deltaR', 'getDeltaR(mu_eta, mu_phi)')
+
 
 df_dimuon.Display(['mu_pt', 'dimuon_mass', 'deltaR']).Print()
 
 #Setup the weights
 df_dimuon = df_dimuon.Define("evt_weight", f'({crossSection}*{luminosity}/{sumWeights})*_weight')
 
+#Lets check the skim
+#print("Plotting the skim inv mass distr")
+#df_dimuon_raw = df_muons.Define('mu_mass', str(muon_massVal))\
+#       .Define('dimuon_mass_skim', 'myInvariantMass(_lPt, _lEta, _lPhi, mu_mass)')
+#cinv = ROOT.TCanvas('cinv', "canvas_invSkim", 800, 600)
+#hinv= df_dimuon_raw.Histo1D(('hinv', "Hist invSkim", 320, 30, 130), 'dimuon_mass_skim')
+#hinv.Draw()
+#cinv.Draw()
+#cinv.Update()
+
+
+
 
 if False:
-	#Print out the efficiencies for each step:
-	totalMuEntries = df_muons.Count().GetValue()
-	eff_triggercut = df_muontrigger.Count().GetValue()/totalMuEntries
-	eff_cuts = df_muons_aftercut.Count().GetValue()/totalMuEntries
-	eff_sel = df_muons_aftercutselection.Count().GetValue()/totalMuEntries
-	eff_dimuon = df_dimuon.Count().GetValue()/totalMuEntries
-	print("\nTotal events with only muons in dataset: " + str(totalMuEntries))
-	print("\nEfficiency after HLTIsoMu27: " + str(eff_triggercut))
-	print("\nEfficiency after cuts: " + str(eff_cuts))
-	print("\nEfficiency after cuts and selection: " + str(eff_sel))
-	print("\nEfficiency after dimuon mass selection: " + str(eff_dimuon))
+    #Print out the efficiencies for each step:
+    totalMuEntries = df_muons.Count().GetValue()
+    eff_triggercut = df_muontrigger.Count().GetValue()/totalMuEntries
+    eff_cuts = df_muons_aftercut.Count().GetValue()/totalMuEntries
+    eff_sel = df_muons_aftercutselection.Count().GetValue()/totalMuEntries
+    eff_dimuon = df_dimuon.Count().GetValue()/totalMuEntries
+    print("\nTotal events with only muons in dataset: " + str(totalMuEntries))
+    print("\nEfficiency after HLTIsoMu27: " + str(eff_triggercut))
+    print("\nEfficiency after cuts: " + str(eff_cuts))
+    print("\nEfficiency after cuts and selection: " + str(eff_sel))
+    print("\nEfficiency after dimuon mass selection: " + str(eff_dimuon))
 
 if (False):
-	c1 = ROOT.TCanvas('c1', 'mu_pT before HLT/Cuts/Selections', 800, 600)
-	h1 = df_muons.Histo1D(('hist_ptmuons', 'mu_pT before HLT/Cuts/Selections', 320, 0, 80), '_lPt')
-	h1.Draw()
+    c1 = ROOT.TCanvas('c1', 'mu_pT before HLT/Cuts/Selections', 800, 600)
+    h1 = df_muons.Histo1D(('hist_ptmuons', 'mu_pT before HLT/Cuts/Selections', 320, 0, 80), '_lPt')
+    h1.Draw()
 
-	c2 = ROOT.TCanvas('c2', 'mu_eta before HLT/Cuts/Selectiosn', 800, 600)
-	h2 = df_muons.Histo1D(('hist_etamuons', 'mu_eta before HLT/Cuts/Selections', 320, -4,4), '_lEta')
-	h2.Draw()
+    c2 = ROOT.TCanvas('c2', 'mu_eta before HLT/Cuts/Selectiosn', 800, 600)
+    h2 = df_muons.Histo1D(('hist_etamuons', 'mu_eta before HLT/Cuts/Selections', 320, -4,4), '_lEta')
+    h2.Draw()
 
-	c3 = ROOT.TCanvas('c3', 'mu_phi before HLT/Cuts/Selectiosn', 800, 600)
-	h3 = df_muons.Histo1D(('hist_phimuons', 'mu_phi before HLT/Cuts/Selections', 320, -4, 4), '_lPhi')
-	h3.Draw()
+    c3 = ROOT.TCanvas('c3', 'mu_phi before HLT/Cuts/Selectiosn', 800, 600)
+    h3 = df_muons.Histo1D(('hist_phimuons', 'mu_phi before HLT/Cuts/Selections', 320, -4, 4), '_lPhi')
+    h3.Draw()
 
-	c1.Draw()
-	c1.Update()
-	c2.Draw()
-	c2.Update()
-	c3.Draw()
-	c3.Update()
+    c1.Draw()
+    c1.Update()
+    c2.Draw()
+    c2.Update()
+    c3.Draw()
+    c3.Update()
 
 #print(df.Filter('HLT_IsoTkMu24').Count().GetValue())
 #print(df_muons.Filter('HLT_IsoTkMu24').Count().GetValue())
 
+#Generate Histograms and put them into a Dictonary
 
-needGeneratePlots = False
+needGeneratePlots = True
 if(needGeneratePlots):
 
-	#generate the plots and save to output file
-	outFile = ROOT.TFile(args.output, "RECREATE")
+    #generate the plots and save to output file
+    outFile = ROOT.TFile(args.output, "RECREATE")
 
-	genTurnOn(df, 'HLT_IsoMu27', 0, 80, 0.1)
-	genTurnOn(df, 'HLT_IsoMu24', 0, 80, 0.1) 
-	genTurnOn(df, 'HLT_IsoTkMu24', 0, 80, 0.1)
-	genPtPlot(df_muons_aftercutselection, 0, 80, 320)
-	genXPlot(df_muons_aftercutselection, 'mu_eta', -4, 4, 320)
-	genXPlot(df_muons_aftercutselection, 'mu_phi', -4, 4, 320)
-	genXPlot(df_dimuon, 'deltaR', 0, 5, 320)
-	genInvMassPlot(df_dimuon, llim_zmass, ulim_zmass, 320)
+    genTurnOn(df, 'HLT_IsoMu27', 0, 80, 0.1)
+    genTurnOn(df, 'HLT_IsoMu24', 0, 80, 0.1) 
+    genTurnOn(df, 'HLT_IsoTkMu24', 0, 80, 0.1)
+    print("\n Generating Pt Plot...")
+    genPtPlot(df_muons_aftercutselection, 0, 80, 320)
+    print("\n Generating Eta Plot...")
+    genXPlot(df_muons_aftercutselection, 'mu_eta', -4, 4, 320)
+    print("\n Generating Phi Plot...")
+    genXPlot(df_muons_aftercutselection, 'mu_phi', -4, 4, 320)
+    print("\n Generating DeltaR Plot...")
+    genXPlot(df_dimuon, 'deltaR', 0, 5, 320)
+    print("\n Generating InvMass Plot...")
+    genInvMassPlot(df_dimuon, llim_zmass, ulim_zmass, 320)
 
-	outFile.Close()
+    outFile.Close()
 
 print("\n Program running... Press Enter to stop.")
 input()  # Waits for the Enter key to be pressed
